092ff8f14e05fd84948ca648db6530a546d835cf
dang0 <7150macNV777DAI@gmail.com>
2025-03-22 13:28:35 +0900

    add: stateManager

---
+++ Assets/Scripts/Character/Friend.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Models;
+using UnityEngine;
+
+namespace Character {
+    public class Friend : MonoBehaviour {
+        [SerializeField] private string animatorParamName = "level";
+        [SerializeField] private InstanceType iType;
+        [SerializeField] private List<FriendAnim> anim;
+
+        private string arn;
+        private string name;
+
+        private Dictionary<InstanceState, int> animDict = new();
+        private Animator animator;
+        
+        
+        void Awake() {
+            foreach (var p in anim) {
+                animDict.Add(p.State, p.AnimP);
+            }
+        }
+
+        private void Start() {
+            animator = GetComponent<Animator>();
+        }
+
+        private void Update() {
+            
+        }
+
+
+        public void InitFriend(string arn, string name) {
+            this.arn = arn;
+            this.name = name;
+        }
+
+        public string Arn => arn;
+
+        public string Name => name;
+
+        public InstanceType IType => iType;
+
+        public void ChangeState(InstanceState state) {
+            animator.SetInteger(animatorParamName, animDict[state]);
+        }
+
+        public void Chat(string prompt) {
+            // TODO Implement later: DO NOT TOUCH!!!!!!
+        }
+
+        void RandomRoutine() {
+            // TODO Implement later: DO NOT TOUCH!!!!!!
+        }
+    }
+
+    [Serializable]
+    public class FriendAnim {
+        [SerializeField] private InstanceState state;
+        [SerializeField] private int animP;
+
+        public InstanceState State => state;
+
+        public int AnimP => animP;
+    }
+}
---
+++ Assets/Scripts/StateManger/StateManger.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Character;
+using Models;
+using UnityEngine;
+
+namespace StateManger {
+    public class StateManger: MonoBehaviour {
+        [SerializeField] private RequestHandler requestHandler;
+        [SerializeField] private List<FriendParams> friendParams;
+        [SerializeField] private InstanceState defaultState;
+        [SerializeField] private float updatePeriod = 0.1f;
+        
+        List<Friend> friends = new();
+        Dictionary<InstanceType, GameObject> prefabs = new();
+        
+        private bool isInitialized = false;
+        private bool isUpdateOccured = false;
+        private bool endRoutine = false;
+
+        private void Awake() {
+            foreach (var p in friendParams) {
+                prefabs.Add(p.IType, p.CharaPrefab);
+            }
+        }
+        
+        private void Start() {
+            StartCoroutine(requestHandler.GetAwsComponents((components) => {
+                foreach (var c in components) {
+                    var friend = prefabs[c.IType].GetComponent<Friend>();
+                    friends.Add(friend);
+                    friend.InitFriend(c.Arn, c.InstanceName);
+                    friend.ChangeState(defaultState);
+                }
+                isInitialized = true;
+            }));
+        }
+
+        void Update() {
+            if (isInitialized && !isUpdateOccured) {
+                endRoutine = false;
+                StartCoroutine(UpdateRoutine());
+                isUpdateOccured = true;
+            }
+        }
+        
+        IEnumerator UpdateRoutine() {
+            if (!isInitialized) {
+                Debug.LogError("Invalid updateRoutine");
+                yield break;
+            }
+
+            while (!endRoutine) {
+                foreach (var friend in friends) {
+                    StartCoroutine(requestHandler.GetAwsState(friend.Arn, state => {
+                        friend.ChangeState(state.IState);
+                    }));
+                }
+                yield return new WaitForSeconds(updatePeriod);
+            }
+        }
+
+        private void OnDestroy() {
+            endRoutine = true;
+        }
+    }
+
+    [Serializable]
+    class FriendParams {
+        [SerializeField] private InstanceType iType;
+        [SerializeField] private GameObject charaPrefab;
+
+        public InstanceType IType => iType;
+
+        public GameObject CharaPrefab => charaPrefab;
+    }
+}
